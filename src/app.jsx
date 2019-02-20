3cca96f0b7e46c4a092c3afc3e65d04c2238c78c
X S <xs@xspro.local>
2019-02-20 23:06:16 +0800

    sp

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,16 @@ import Button from 'lime/Button'
 import Select from 'lime/Select'
 import Scroll from 'lime/Scroll'
 import List from 'lime/List'
+import Layer from 'lime/Layer'
 import 'lime/lime.css'
 
-const options = [
-    { key: 'a', text: 'a' }
-]
+const options = []
+for (let i = 0; i < 20; i++) {
+    options.push({
+        key: i,
+        text: 'a' + i
+    })
+}
 
 const optionsII = [
     { key: 'a', text: 'a' },
@@ -24,9 +29,46 @@ const optionsII = [
     { key: 'a', text: 'a' }
 ]
 
+class S extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    onClick = evt => {
+        let { left, top, width, height } = evt.target.getBoundingClientRect()
+        this.setState({
+            left,
+            top: top + height,
+            width,
+            show: true
+        })
+    }
+
+    onBlur = evt => {
+        this.setState({ show: false })
+    }
+
+    render() {
+        let { left, top, width, show } = this.state
+        return <div>
+            <input onClick={this.onClick} />
+            <Layer
+                left={left}
+                top={top}
+                show={show}
+                onBlur={this.onBlur}
+                width={width}
+            >
+                <List items={options.map(i => i.text)} />
+            </Layer>
+        </div>
+    }
+}
+
 export default class App extends React.Component {
     render() {
-        return <div>
+        return <div style={{  }}>
             <div><Button>Lime</Button></div>
             <div>
                 <Select options={options} />
@@ -42,6 +84,24 @@ export default class App extends React.Component {
                     <option>4</option>
                 </select>
             </div>
+
+            {/* <div className='sd-layer-mask' onMouseLeave={()=>console.log(`leave`,)}></div>
+            <div className='sd-layer'>
+                <List items={options.map(i => i.text)} />
+            </div> */}
+            <div>
+                <S />
+            </div>
+            {/* <div>
+                <Layer
+                    left={100}
+                    top={400}
+                    show={true}
+                    width={300}
+                >
+                    <List items={options.map(i => i.text)} />
+                </Layer>
+            </div> */}
         </div>
     }
 }
