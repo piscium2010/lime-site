b09182d7a09ee68f99ead1b4ba8a7dcc5cbdbdc9
X S <xs@xspro.local>
2019-02-22 00:21:42 +0800

    sp

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import Scroll from 'lime/Scroll'
 import List from 'lime/List'
 import Layer from 'lime/Layer'
 import 'lime/lime.css'
+import './app.less'
 
 const options = []
 for (let i = 0; i < 20; i++) {
@@ -39,7 +40,7 @@ class S extends React.Component {
         let { left, top, width, height } = evt.target.getBoundingClientRect()
         this.setState({
             left,
-            top: top + height,
+            top: top + height + 5,
             width,
             show: true
         })
@@ -49,10 +50,14 @@ class S extends React.Component {
         this.setState({ show: false })
     }
 
+    onClickItem = evt => {
+        this.setState({ value: evt.target.textContent, show: false })
+    }
+
     render() {
-        let { left, top, width, show } = this.state
-        return <div>
-            <input onClick={this.onClick} />
+        let { left, top, width, show, value } = this.state
+        return <div className='sd-select-input'>
+            <input onClick={this.onClick} value={value} />
             <Layer
                 left={left}
                 top={top}
@@ -60,7 +65,14 @@ class S extends React.Component {
                 onBlur={this.onBlur}
                 width={width}
             >
-                <List items={options.map(i => i.text)} />
+                <List
+                    items={options}
+                    renderItem={it => {
+                        return (
+                            <div className='test' style={{ padding: 5 }} onClickCapture={this.onClickItem}>{it.text}</div>
+                        )
+                    }}
+                />
             </Layer>
         </div>
     }
@@ -68,14 +80,26 @@ class S extends React.Component {
 
 export default class App extends React.Component {
     render() {
-        return <div style={{  }}>
+        return <div style={{}}>
+       
             <div><Button>Lime</Button></div>
+            <br/>
+            <br/>
             <div>
                 <Select options={options} />
             </div>
+            <br/>
+            <br/>
             <div>
                 <Select options={optionsII} />
             </div>
+            <br/>
+            <br/>
+            <div>
+                <S />
+            </div>
+            <br/>
+            <br/>
             <div>
                 <select>
                     <option>1</option>
@@ -84,14 +108,16 @@ export default class App extends React.Component {
                     <option>4</option>
                 </select>
             </div>
-
+            <br/>
+            <br/>
+            <div>
+                <List items={options.map(i => i.text)} />
+            </div>
             {/* <div className='sd-layer-mask' onMouseLeave={()=>console.log(`leave`,)}></div>
             <div className='sd-layer'>
                 <List items={options.map(i => i.text)} />
             </div> */}
-            <div>
-                <S />
-            </div>
+            
             {/* <div>
                 <Layer
                     left={100}
