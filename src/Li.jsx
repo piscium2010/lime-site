d61afec1923b33f421b620117c8f621a5fb18104
Xiaobin <xshen1@statestreet.com>
2019-03-01 17:53:48 +0800

    sp

--- src/Li.jsx
+++ src/Li.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import Layer from 'lime/Layer'
+import debounce from 'debounce'
 
 export default class Li extends React.Component {
     constructor(props) {
         super(props);
         this.state = { show: false }
+        this.debouncedOnMouseOver = debounce(this.onMouseOver, 200)
+        this.debouncedOnMouseLeave = debounce(this.onMouseLeave, 200)
     }
 
-    onClick = evt => {
+    onMouseOver = evt => {
+        if (evt.target !== evt.currentTarget) return
+        console.log(`over`, Math.random())
         let { children } = this.props
         let { left, top, width, height } = evt.target.getBoundingClientRect()
         this.setState({
             show: children ? true : false,
             left: left + width,
             top
+        })
+    }
+
+    onMouseLeave = evt => {
+        console.log(`leave`)
+        this.setState({
+            show: false
         })
     }
 
@@ -28,14 +40,15 @@ export default class Li extends React.Component {
         let { show, left, top } = this.state
         let { children, title } = this.props
         return (
-            <React.Fragment>
-                <li style={style} onClick={this.onClick}>{title}</li>
+            <li style={style} onMouseOver={this.onMouseOver} onMouseLeave={this.onMouseLeave}>
+                {title}
                 <Layer show={show} left={left} top={top} onBlur={this.onBlurLayer}>
                     {
                         children
                     }
                 </Layer>
-            </React.Fragment>
+            </li>
+
         )
     }
 }
